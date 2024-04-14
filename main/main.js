3454d9417ac6199e51834d92a146ae4b6b6eb0d7
belyj_ai <belyj_ai@teleformis.ru>
2024-04-14 09:15:19 +0300

    created new angular app with electron

---
+++ main/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron/main');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+
+  void win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
