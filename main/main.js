8b3d8d6b5d119253be4d9bd1003b00a2ef5e6be2
belyj_ai <belyj_ai@teleformis.ru>
2024-04-15 08:47:38 +0300

    wrote script 'start' for starting app concurrently

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 const { app, BrowserWindow } = require('electron/main');
+const path = require("path");
+const url = require("url");
+
+const isDev = process.argv.slice(1).some((val) => val === "--dev");
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -10,7 +14,25 @@ function createWindow () {
     }
   })
 
-  void win.loadFile('index.html')
+  if (isDev) {
+    void win.loadURL(
+      url.format({
+        pathname: "localhost:4200",
+        protocol: "http:",
+        slashes: true,
+      })
+    );
+
+    win.webContents.openDevTools();
+  } else {
+    void win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "../dist/subd/index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    );
+  }
 }
 
 app.whenReady().then(() => {
