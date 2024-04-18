68658f438334295c09bdbf71568126bd6010ef3c
belyj_ai <belyj_ai@teleformis.ru>
2024-04-18 08:30:22 +0300

    created standalone main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'electron-app-with-cassandra';
-}
+export class AppComponent {}
