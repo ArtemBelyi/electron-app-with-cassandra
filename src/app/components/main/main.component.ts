db2441cef43c70cde617043b353db08c09516b5b
belyj_ai <belyj_ai@teleformis.ru>
2024-05-19 21:36:00 +0300

    removed uninstalled cassandra driver and removed cassandra service

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CassandraService } from "../../services/cassandra/cassandra.service";
 import { AstraDatabaseService } from "../../services/astra-database/astra-database.service";
 
 @Component({
@@ -10,32 +9,9 @@ import { AstraDatabaseService } from "../../services/astra-database/astra-databa
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-  constructor(
-    readonly cassandraService: CassandraService,
-    readonly astraDBService: AstraDatabaseService
-  ) {}
-
-  testConnection(): void {
-    this.cassandraService.testConnection().then(res => console.log(res))
-  }
-
-  createKeyspace(): void {
-    this.cassandraService.createKeyspace("new_keyspace_belyi_1").then(res => console.log(res))
-  }
+  constructor(readonly astraDBService: AstraDatabaseService) {}
 
   createConnect(): void {
     this.astraDBService.connect().then(res => console.log(res))
-  }
-
-  createTable(): void {
-    this.cassandraService.createTable("abelyi_2", "Table_race_2").then(res => console.log(res))
-  }
-
-  getTablesName(): void {
-    this.cassandraService.getTableNamesFromKeyspace("abelyi_2").then(res => console.log(res))
-  }
-
-  getColumnFromTable(): void {
-    this.cassandraService.getColumnsForTable("abelyi_2", "table_race_2").then(res => console.log(res))
   }
 }
