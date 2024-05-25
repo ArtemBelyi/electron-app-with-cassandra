0fac52b3625011412eea039a756c96dad244c246
belyj_ai <belyj_ai@teleformis.ru>
2024-05-25 22:50:56 +0300

    removed variables

--- main/main.js
+++ main/main.js
@@ -2,13 +2,7 @@ const { app, BrowserWindow } = require('electron/main');
 
 const isDev = process.argv.slice(1).some((val) => val === "--dev");
 const path = require("path");
-const url = require("url");
-
-process.env.ASTRA_DB_APPLICATION_TOKEN_1 = "AstraCS:gwUPAQWUYwZqAXeKNpXgPkAQ:04645b5637c4ae509a1b7ba755ccb2f57bed70106924d3ef936e56e4c73cf7e0";
-process.env.ASTRA_DB_SECURE_BUNDLE_PATH_1 = "src/app/services/cassandra/credentials/secure-connect-db-app-1.zip";
-
-process.env.ASTRA_DB_APPLICATION_TOKEN_2 = "AstraCS:EGogjFdtGWxAteJOSFsKRibD:efd69450d1bd7e81a20f30ad33fcc29af5cea59cd4bb6996c1c734b830d4242c";
-process.env.ASTRA_DB_SECURE_BUNDLE_PATH_2 = "src/app/services/cassandra/credentials/secure-connect-db-app-2.zip";
+const url = require("url");;
 
 function createWindow () {
   const win = new BrowserWindow({
--- src/app/services/astra-database/astra-database.service.ts
+++ src/app/services/astra-database/astra-database.service.ts
@@ -10,7 +10,7 @@ export class AstraDatabaseService {
 
   async connect(): Promise<DatabaseInfo> {
     const client = new DataAPIClient('AstraCS:gwUPAQWUYwZqAXeKNpXgPkAQ:04645b5637c4ae509a1b7ba755ccb2f57bed70106924d3ef936e56e4c73cf7e0');
-    const db2 = client.db('c0652eb7-52d3-4ed7-870c-d2baab5a68eb', 'us-east1');
+    const db2 = client.db('dabd8e07-c668-44c5-9ee6-b5cd1e5350d6', 'us-east1');
 
     try {
       return await db2.info();
